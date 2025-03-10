15c0d3b2d616dae730a3e0d6b6002712a7169ef7
Thanh Giang Nguyen Tan <64969412+nttg8100@users.noreply.github.com>
2025-03-10 20:15:52 +0700

    simplify SLURM cluster (#26)
    * remove support single node
    
    * update document, rewrite playbook and inventories

--- roles/nextflow-demo/files/fetchngs/fetchngs.sh
+++
@@ -1,8 +0,0 @@
-nextflow run nf-core/fetchngs -r 1.12.0 \
--profile singularity \
---max_cpus 2 \
---max_memory 2GB \
---max_time 1h \
---outdir fetchngs/output \
--work-dir fetchngs/work \
---input fetchngs/inputs.csv
